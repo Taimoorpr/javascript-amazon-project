7040b2ca6a630026ea545ba79362db8faa6e9cc1
Taimoor <taimoorpro76@gmail.com>
2023-08-01 01:25:44 -0700

    import products from products.js

--- data/products.js
+++ data/products.js
@@ -1,4 +1,4 @@
-const products = [{
+export const products = [{
         id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         image: "images/products/athletic-cotton-socks-6-pairs.jpg",
         name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -49,7 +49,7 @@ const products = [{
             "stars": 5,
             "count": 2088
         },
-        "priceCents": 3103,
+        "priceCents": 3155,
         "keywords": [
             "Safe",
             "hoodie"
